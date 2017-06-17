c90f724c648400cab1324b9f24b61659789350dd
dengdan <dengdan890730@163.com>
2017-06-17 19:14:40 +0800

    The first commit

---
+++ python/test_nametuple.py
@@ -0,0 +1,26 @@
+import collections
+import util
+util.init_logger()
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = range(2, 11) + list("JQKA")
+    suits = 'spade diamond heart club'.split()
+    
+    def __init__(self):
+        self.cards = [Card(r, s) for r in self.ranks for s in self.suits]
+        
+    @util.dec.print_calling_in_short
+    def __len__(self):
+        return len(self.cards);
+    
+    @ util.dec.print_calling_in_short
+    def __getitem__(self, position):
+        return self.cards[position];
+    
+cards = FrenchDeck()
+print len(cards)
+print cards[0]
+
+import random
+print random.choice(cards)
